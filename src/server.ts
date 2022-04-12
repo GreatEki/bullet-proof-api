78cd2dedc89d1fded54fba0a10abb5eeb6f50fba
GreatEki <ekeneidiagbor@gmail.com>
2022-04-12 12:20:29 +0100

    chore: express server setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Node')
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv'
+import path from 'path';
+
+const app = express();
+
+// Configure global variables
+dotenv.config({path:  path.resolve(__dirname, './config/config.env')})
+
+const PORT = process.env.PORT || 9000
+
+app.listen(PORT, () => console.log(`App running in ${process.env.NODE_ENV} mode on PORT ${PORT}`))
