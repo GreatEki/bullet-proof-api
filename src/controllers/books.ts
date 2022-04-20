00f80bb504617f4a506dee460092ef4af70e6bdb
GreatEki <ekeneidiagbor@gmail.com>
2022-04-20 09:46:12 +0100

    feat: added endpoint to createBook

--- src/controllers/books.ts
+++ src/controllers/books.ts
@@ -1,5 +1,49 @@
 import { NextFunction, Request, Response } from 'express';
 import { Book } from '../models';
+import mongoose from 'mongoose';
+
+
+const createBook = async (req: Request, res: Response) => {
+    try {
+
+        let { author, title} = req.body;
+
+        if (!author) {
+            return res.status(401).json({
+                success: false,
+                message: "Author field is required"
+            })
+        }
+
+        if (!title) {
+            return res.status(401).json({
+                success: false,
+                message: 'Title field is required'
+            })
+        }
+
+        // post book
+        const book = new Book({
+            _id: new mongoose.Types.ObjectId(),
+            author,
+            title
+        })
+
+        const newBook = await book.save();
+
+        return res.status(201).json({
+            success: true,
+            message: 'Book added successfully',
+            newBook: newBook
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message,
+            error: err
+        })
+    }
+}
 
 const getAllBooks = async (req: Request, res: Response, next: NextFunction) => {
 
@@ -23,4 +67,4 @@ const getAllBooks = async (req: Request, res: Response, next: NextFunction) => {
 
   
 }
-export  { getAllBooks }
+export  { getAllBooks, createBook }
--- src/routes/books.ts
+++ src/routes/books.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getAllBooks } from '../controllers/books';
+import { getAllBooks, createBook } from '../controllers/books';
 
 const router = express.Router();
 
 router.route('/getAllBooks').get(getAllBooks)
+
+router.route('/createBook').post(createBook)
 
 
 export default router;
